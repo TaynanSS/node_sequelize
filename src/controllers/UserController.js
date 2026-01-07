0b155e02f20353e1d975e64ca25210496e6eb5c4
TaynanSS <taynansilvass2@gmail.com>
2026-01-07 14:36:48 -0300

    Finalização do CRUD, implementando os métodos UPDATE e DELETE. Feito testes de atualização e exclusão de user do BD com Insomnia

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -12,18 +12,59 @@ export default {
         return res.status(200).send({ users });
     },
 
-    // store: Salvar 
+    // store: Salvar/Inserir informações dentro do banco.
     async store(req, res) {
+        const { name, password, email } = req.body; // Puxando as informações de forma desestruturada.
 
+        const user = await User.create({ name, password, email });
+
+        return res.status(200).send({
+            status: 1,
+            message: 'Usuário cadastrado com sucesso',
+            user
+        })
     },
 
     // update: Atualizar
     async update(req, res) {
+        const { name, password, email } = req.body;
+        const { user_id } = req.params;
+
+        const [rows_affected] = await User.update({
+            name, password, email
+        }, {
+            where: {
+                id: user_id
+            }
+        });
+
+        if (rows_affected === 0) {
+            return res.status(404).send({
+                status: 0,
+                message: "Usuário não encontrado ou nenhuma linha afetada."
+            })
+        }
+
+        return res.status(200).send({
+            status: 1,
+            message: "Usuário atualizado com sucesso!",
+        });
 
     },
 
     // delete: Deletar
     async delete(req, res) {
+        const { user_id } = req.params;
 
-    }
+        await User.destroy({
+            where: {
+                id: user_id
+            }
+        })
+
+        return res.status(200).send({
+            status: 1,
+            message: "Usuário deletado com sucesso!"
+        });
+    },
 };
