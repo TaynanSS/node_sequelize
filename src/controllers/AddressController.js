083c935576302b744abe287207b6de84ff0993c4
TaynanSS <taynansilvass2@gmail.com>
2026-01-08 14:23:07 -0300

    Add nova table + association tables with foreignkey

---
+++ src/controllers/AddressController.js
@@ -0,0 +1,115 @@
+import User from '../models/User';
+import Address from '../models/Address';
+
+
+export default {
+    async index(req, res) {
+        const { user_id } = req.params;
+
+        const user = await User.findByPk(user_id, {
+            include: { association: 'address'}
+        });
+
+        if (!user) {
+            return res.status(400).send({
+                status: 0,
+                message: 'Produtos não encontrado!'
+            });
+        }
+
+        return res.status(200).send(user.address);
+    },
+
+    async store(req, res) {
+
+      try {
+
+          const { user_id } = req.params;
+          const { street, number, district, city } = req.body;
+
+          const user = await User.findByPk(user_id);
+
+          if (!user) {
+              return res.status(400).json({
+                  status: 0,
+                  message: 'Usuário não encontrado!'
+              });
+          }
+
+          const address = await Address.create({
+            street,
+            number,
+            district,
+            city,
+            user_id,
+          });
+
+          return res.status(200).json({
+              status: 1,
+              message: "Address cadastrado com sucesso!",
+              address
+          });
+
+        } catch (err) {
+          return res.status(400).json({ error: err });
+      }
+    },
+
+    async delete(req, res) {
+        const id = req.params.id;
+
+        try {
+            const address = await Address.findByPk(id);
+
+            if (address) {
+                await Address.destroy({ where: { id } });
+
+                return res.status(200).json({
+                    status: 1,
+                    message: "Address apagado com sucesso!",
+                });
+
+            } else {
+                return res.status(400).json({
+                    status: 0,
+                    message: 'Address não encontrado!'
+                });
+            }
+
+
+        } catch (err) {
+            return res.status(400).json({ error: err });
+        }
+    },
+
+    async update(req, res) {
+        const id = req.params.id;
+        const { street, number, district, city } = req.body;
+
+        try {
+            const address = await Address.findByPk(id);
+
+            if (address) {
+                await Address.update({ street, number, district, city }, { where: { id } });
+
+                return res.status(200).json({
+                    status: 1,
+                    message: "Address atualizado com sucesso!",
+                });
+
+            } else {
+                return res.status(400).json({
+                    status: 0,
+                    message: 'Address não encontrado!'
+                });
+            }
+
+
+        } catch (err) {
+            return res.status(400).json({
+                status: 0,
+                message: 'Erro ao atualizar Address!'
+            });
+        }
+    }
+};
---
+++ src/database/migrations/20260108162707-create-addresses.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+export default {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.createTable('addresses', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key:'id' }, //informa que é um foreighkey do id da tabela users e
+        onUpdate: 'CASCADE', // quando deletar ou atualizar os dados, irá fazer em todos os dados associados a ele.
+        onDelete: 'CASCADE',
+      },
+
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      district: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+
+    });
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('addresses');
+    
+  }
+};
